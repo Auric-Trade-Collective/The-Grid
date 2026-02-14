c8790902d5b09bf42c3354fa26f0ad899532664a
Jinx <jinxynet@duck.com>
2026-02-14 13:25:22 +0100

    chore: Boilerplate for test CRUD API

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/ping", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run()
+}
